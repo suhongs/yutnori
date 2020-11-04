2e2e4ad0e4b7677a6de8b105a6885c395a56cebe
Jimin <57743122+kaley0421@users.noreply.github.com>
2020-11-04 15:24:54 +0900

    Add files via upload
    Player, Piece, Move class

---
+++ Move.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp1
+{
+    public enum Move
+    {
+        STACK,          //내 말에 다른 내 말을 쌓는다
+        CAPTURE,        //다른 팀 말 잡기
+        NORMAL          //STACK 도, CAPTURE 도 아닌 일반적 이동
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
